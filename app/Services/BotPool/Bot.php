2a8d3412ade91d56c8fb7776365168cc9ce3101a
Andriy Matviichuk <andrei.matviichuk@ning.com>
2018-10-12 14:17:54 +0300

    genome and bot prototype

--- app/Services/BotPool/Bot.php
+++ app/Services/BotPool/Bot.php
@@ -4,8 +4,31 @@ namespace App\Services\BotPool;
 
 class Bot
 {
-    private $energi = 0;
+    private $energi;
     private $latitude;
     private $longitude;
-    private $genomPoint = 0;
+    private $genome;
+
+    private function setCoordinates(int $latitude, int $longitude):void
+    {
+        $this->latitude = $latitude;
+        $this->longitude = $longitude;
+    }
+
+    private function setGenomeMutation(): void
+    {
+        if (mt_rand(1,4) >= 4) {
+            $this->genome->mutate();
+        }
+    }
+
+    public function __construct(Genome $genome, $latitude = 0, $longitude = 0)
+    {
+        $this->energi = 0;
+        $this->setCoordinates($latitude, $longitude);
+        $this->genome = $genome;
+        $this->setGenomeMutation();
+    }
+
+
 }
